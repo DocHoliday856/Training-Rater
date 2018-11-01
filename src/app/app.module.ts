1dd59417cf175d7f858fbcbfe2275ad530887c4b
m t <Shawn.Dougherty.NJ@Gmail.com>
2018-11-01 19:11:42 -0400

    Session 2 First Commit / Added RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { SessionsService } from './sessions-list/sessions.service';
+import { HomeComponent } from './home/home.component';
+import { SessionsListComponent } from './sessions-list/sessions-list.component';
+import { AppRoutingModule } from './app-routing-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SessionsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [SessionsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
